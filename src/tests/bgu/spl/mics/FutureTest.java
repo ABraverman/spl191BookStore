1f2277d9eee3d4574a454a8f745e062bba62b28d
ABraverman <40732067+ABraverman@users.noreply.github.com>
2018-11-25 21:15:55 +0200

    added a test to future

---
+++ src/tests/bgu/spl/mics/FutureTest.java
@@ -0,0 +1,29 @@
+package bgu.spl.mics;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FutureTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void resolve() {
+    }
+
+    @Test
+    public void isDone() {
+    }
+
+    @Test
+    public void get1() {
+    }
+}
