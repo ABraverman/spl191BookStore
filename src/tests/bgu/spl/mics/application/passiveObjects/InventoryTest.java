3dc045c1dabe1bc3cf660a6e49c5b3673ea19e76
ABraverman <40732067+ABraverman@users.noreply.github.com>
2018-11-25 21:09:04 +0200

    added a test to inventory

---
+++ src/tests/bgu/spl/mics/application/passiveObjects/InventoryTest.java
@@ -0,0 +1,28 @@
+package bgu.spl.mics.application.passiveObjects;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InventoryTest {
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void load() {
+    }
+
+    @Test
+    public void take() {
+    }
+
+    @Test
+    public void checkAvailabiltyAndGetPrice() {
+    }
+
+    @Test
+    public void printInventoryToFile() {
+    }
+}
